f22423adc9a9fb3b5ab9a65ba2b3b8e95f63f380
AngeloC89 <Angelociulla89@gmail.com>
2024-06-12 19:46:06 +0200

    change on index api controller the  function from all to pagianate

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -6,13 +6,16 @@ use App\Models\Project;
 
 use App\Http\Controllers\Controller;
 use GrahamCampbell\ResultType\Success;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
+
+
 
 class ProjectController extends Controller
 {
     public function index()
     {
-        $projects = Project::all();
+        $projects = Project::paginate(4);
         return response()->json([
             'status' => 'success',
             'message' => 'ok',
