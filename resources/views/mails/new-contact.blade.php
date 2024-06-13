6bf3229fa9132f1ada3d58fe35717a76b634fbcc
AngeloC89 <Angelociulla89@gmail.com>
2024-06-13 19:07:51 +0200

    made migration of the lead_table

---
+++ resources/views/mails/new-contact.blade.php
@@ -0,0 +1,7 @@
+<h1>All'attenzione dell'amministratore</h1>
+
+<p>Hai ricevuto un nuovo messaggio: <br>
+<strong>Name:</strong><span>{{$data->name}}</span><br>
+<strong>Email:</strong><span>{{$data->address}}</span><br>
+<strong>Message:</strong><span>{{$data->message}}</span><br>
+</p>
