e293b3d4cccd2f89cc06d1b080d24e35236bb220
AngeloC89 <Angelociulla89@gmail.com>
2024-10-11 17:28:58 +0200

    Test seeders performed

--- database/seeders/ProjectTableSeeder.php
+++ database/seeders/ProjectTableSeeder.php
@@ -32,7 +32,6 @@ class ProjectTableSeeder extends Seeder
                 $new_project = new Project();
                 $new_project->title = $project[0];
                 $new_project->content = $project[1];
-                $new_project->image = $project[2];
                 $new_project->slug = Project::generateSlug($new_project->title);
                 $new_project->save();
             }
