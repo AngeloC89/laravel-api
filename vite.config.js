be6e030527706179ecc003d5139b73a540c5397c
AngeloC89 <Angelociulla89@gmail.com>
2024-11-09 23:38:45 +0100

    try deploy

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,35 @@
-import {
-    defineConfig
-} from 'vite';
-import laravel from 'laravel-vite-plugin';
-import path from 'path'; // <-- require path from node
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import path from "path"; // <-- require path from node
 
 export default defineConfig({
     plugins: [
         laravel({
             // edit the first value of the array input to point to our new sass files and folder.
-            input: ['resources/scss/app.scss', 'resources/js/app.js'],
+            input: ["resources/scss/app.scss", "resources/js/app.js"],
             refresh: true,
         }),
     ],
     // Add resolve object and aliases
     resolve: {
         alias: {
-            '~icons': path.resolve(__dirname, 'node_modules/bootstrap-icons/font'),
-            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+            "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
             "~@fortawesome": path.resolve(
                 __dirname,
                 "node_modules/@fortawesome"
             ),
-            '~resources': '/resources/'
-            
-        }
-    }
+            "~icons": path.resolve(
+                __dirname,
+                "node_modules/bootstrap-icons/font"
+            ),
+            "~resources": path.resolve(__dirname, "resources"),
+        },
+    },
+    server: {
+        https: true, // Assicurati che Vite usi HTTPS in fase di sviluppo
+    },
+    build: {
+        // Opzione per forzare gli asset generati a essere in HTTPS in produzione
+        assetPublicPath: "/build/", // Non dovresti aver bisogno di modificarlo se stai usando il path corretto in Laravel
+    },
 });
