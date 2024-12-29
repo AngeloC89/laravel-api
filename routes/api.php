82b9d1b11b593f96b5ef4aae0b0bee5fb36e00ab
AngeloC89 <Angelociulla89@gmail.com>
2024-12-29 20:42:20 +0100

    fix api router leads

--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,7 @@ Route::get('projects', [ProjectController::class, 'index']);
 Route::get('projects/{slug}', [ProjectController::class, 'show']);
 Route::get('technologies', [TechnologyController::class, 'index']);
 
-Route::post('contacts', [LeadController::class, 'store']);
+Route::post('leads', [LeadController::class, 'store']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
